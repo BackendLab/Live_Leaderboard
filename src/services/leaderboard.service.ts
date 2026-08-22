65c14f8635f8352763f533e824c16aac7fa257c9
Akhil-Nagpal <akhilnagpal07@gmail.com>
2026-08-22 21:12:00 +0530

    feat: implemented getLeaderboardService to get top10 with  own standings

--- src/services/leaderboard.service.ts
+++ src/services/leaderboard.service.ts
@@ -1,6 +1,48 @@
 import { redis } from "../db/connect";
 import { User } from "../models/user.model";
 
+type LeaderboardEntry = {
+  userId: string | undefined;
+  score: number;
+  isCurrentUser?: boolean;
+};
+
+export const getLeaderboardService = async (userId: string | undefined) => {
+  // fetch the top 10 from leaderboard
+  const rawLearderboard = await redis.zrevrange(
+    "leaderboard",
+    0,
+    9,
+    "WITHSCORES",
+  );
+  // convert the string into json objects with pairs
+  const top10: LeaderboardEntry[] = [];
+  for (let i = 0; i < rawLearderboard.length; i += 2) {
+    top10.push({
+      userId: rawLearderboard[i],
+      score: Number(rawLearderboard[i + 1]),
+    });
+  }
+  // get the own entry
+  let ownEntry: { userId: string; rank: number; score: number } | null = null;
+  // check if user is in the top 10 get the rank
+  if (userId) {
+    const rank = await redis.zrevrank("leaderboard", userId);
+    // after getting the rank check if the user is in top 10, if yes - then highlight that with adding new field
+    if (rank !== null && rank <= 10) {
+      if (top10[rank]) {
+        top10[rank].isCurrentUser = true;
+      }
+    } else if (rank !== null) {
+      // if not in top 10 the fetch the score and update the ownEntry variable
+      const score = await redis.zscore("leaderboard", userId);
+      ownEntry = { userId, rank: rank + 1, score: Number(score) };
+    }
+  }
+  // return top 10 and own entry
+  return { top10, ownEntry };
+};
+
 export const registerUserService = async (
   username: string,
   fullName: string,
